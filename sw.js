447d3d6985c4952a999c8fb68a10c19e288680b9
hamchorom-jeju <moonjin05@gmail.com>
2026-05-17 00:14:05 +0900

    fix: Fine-tune bottom padding for balanced UX v44.233

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for Nohyung Jumping PWA (v44.117)
 // Strategy: Network First for HTML, Cache First for assets
 
-const CACHE_NAME = 'nohyung-jumping-v44.232';
+const CACHE_NAME = 'nohyung-jumping-v44.233';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
